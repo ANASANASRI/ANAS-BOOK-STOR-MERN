8e5cfcc9d6df0e3145f641764104229e0c9e0de0
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-01-18 21:06:40 +0100

    header

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import header from "../assets/headerCss.css"
 
 function Header() {
         return <div>
                     <nav>
                     <ul>
                         <li><Link to={"/"}>Home</Link></li>
-                        <li><Link to={"/"}>Home</Link></li>
-                        <li><Link to={"/"}>Home</Link></li>
-                        <li><Link to={"/"}>Home</Link></li>
+                        <li><Link to={"/books"}>Books</Link></li>
+                        <li><Link to={"/add"}>AddBooks</Link></li>
+                        <li><Link to={"/"}>About</Link></li>
                     </ul>
                     </nav>
                 </div>
