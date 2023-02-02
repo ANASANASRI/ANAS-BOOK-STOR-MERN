aa4882b8d3a05584b3f2d0fa7984e24748928990
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-02-02 18:50:43 +0100

    add user icon to header

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom"
 import header from "../assets/headerCss.css"
 import logo from "../assets/logo.png"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 function Header() {
         return <div>
                     <nav>
                     <ul>
-                        <Link to={"/"}><img alt="logo" src={logo} className="logo"/></Link>
+                        <li><Link to={"/"}><img alt="logo" src={logo} className="logo"/></Link></li>
                         <li><Link to={"/"}>Home</Link></li>
                         <li><Link to={"/books"}>Books</Link></li>
                         {/* <li><Link to={"/add"}>AddBooks</Link></li> */}
+                        <li><Link to={"/login"}><FontAwesomeIcon icon={faUser} /></Link></li>
                     </ul>
                     </nav>
                 </div>
