7f3e65e06924ccb90dbf78b36d4ff9780d468e17
ANAS ANASRI <100525138+ANASRIANAS@users.noreply.github.com>
2023-02-02 19:30:17 +0100

    add login page and logincss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom"
 import Home from './Components/Home';
+import Login from './Components/Login';
 import Header from './Components/Header';
 import BooksListe from './Components/BooksListe';
 import AddBookForm from './Components/AddBookForm';
@@ -12,6 +13,7 @@ function App() {
     <Header />
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
       <Route path='/books' element={<BooksListe/>}/>
       <Route path='/add' element={<AddBookForm/>}/>
       <Route path='/books/:id' element={<DetailBook/>}/>
---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,18 @@
+import { useState } from "react" 
+import { Link } from "react-router-dom"
+import Logincss from "../assets/loginCss.css"
+
+function Login() {
+    return(
+        <div class="login">
+            <h1>Login</h1>
+            <form method="post">
+                <input type="text" name="u" placeholder="Username" required="required" />
+                <input type="password" name="p" placeholder="Password" required="required" />
+                <button type="submit" class="btn btn-primary btn-block btn-large">Let me in</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login
